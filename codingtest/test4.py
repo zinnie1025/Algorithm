7a6958c807432000d13114f543cf727abc47a794
zinnie1025 <luckyjj1025@gmail.com>
2024-02-15 19:51:23 +0900

    240215

---
+++ codingtest/test4.py
@@ -0,0 +1,7 @@
+
+#* 신규 아이디 추천
+from typing import List
+
+new_id = ["...!@BaT#*..y.abcdefghijklm", "z-+.^.", "=.=", "123_.def", "abcdefghijklmn.p"]
+result = ["bat.y.abcdefghi", "z--",  "aaa", 	"123_.def", "abcdefghijklmn"]
+
